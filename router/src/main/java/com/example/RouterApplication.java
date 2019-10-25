b720759bdfd4db7465df8c988e362ae8a439633f
bian.qebjx <784930683@qq.com>
2019-10-25 15:50:16 +0800

    test commit and push

--- router/src/main/java/com/example/RouterApplication.java
+++ router/src/main/java/com/example/RouterApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy  //表明该服务利用zuul实现路由功能
+//test
 public class RouterApplication {
     public static void main(String[] args) {
         SpringApplication.run(RouterApplication.class,args);
